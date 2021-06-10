cff12e6da7d558a51c87bfc2220da4577132795a
RaptorTech4 <bennet.rumboll@gmail.com>
2021-06-10 13:13:33 +0200

    ml brain

--- UnityProjectFiles/KartProject/venv/Scripts/activate.bat
+++ UnityProjectFiles/KartProject/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\benne\OneDrive\Documents\aie\Assessment_4_Racing_Game\UnityProjectFiles\KartProject\venv
+set VIRTUAL_ENV=D:\Projects\Assessment_4_Racing_Game\UnityProjectFiles\KartProject\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
